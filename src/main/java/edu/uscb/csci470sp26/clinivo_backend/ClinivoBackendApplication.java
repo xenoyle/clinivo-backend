812455f2cfd87097067a8b88748c9f2923bfbc1d
Connor Floyd <connorwfloyd@gmail.com>
2026-03-30 12:10:22 -0400

    initial commit

---
+++ src/main/java/edu/uscb/csci470sp26/clinivo_backend/ClinivoBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci470sp26.clinivo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinivoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinivoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uscb/csci470sp26/clinivo_backend/ClinivoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci470sp26.clinivo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinivoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
