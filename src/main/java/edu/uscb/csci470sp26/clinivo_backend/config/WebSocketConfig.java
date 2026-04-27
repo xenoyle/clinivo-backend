5071db6240422f1a1d7e8fcc7e9ac1af86ec179e
Connor Floyd <cwfloyd@email.uscb.edu>
2026-04-26 22:06:02 -0400

    Adapted cors config and web socket config and added a Dockerfile for deployment on Render

--- src/main/java/edu/uscb/csci470sp26/clinivo_backend/config/WebSocketConfig.java
+++ src/main/java/edu/uscb/csci470sp26/clinivo_backend/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package edu.uscb.csci470sp26.clinivo_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,12 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
-    }
+	// Inject the allowed origin from environment variables
+	@Value("${cors.allowed-origin:http://localhost:5173}")
+	private String allowedOrigin;
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+	    registry.addEndpoint("/ws")
+	            .setAllowedOrigins(allowedOrigin) // Explicitly trust Netlify
+	            .withSockJS();
+	}
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
