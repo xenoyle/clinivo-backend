c0eb8e422fa48048190bd253b856d8488ea8d914
Connor Floyd <cwfloyd@email.uscb.edu>
2026-05-03 15:22:31 -0400

    Added a check for doctor in database post-build and added a findByRole method in UserRepository.java

--- src/main/java/edu/uscb/csci470sp26/clinivo_backend/repository/UserRepository.java
+++ src/main/java/edu/uscb/csci470sp26/clinivo_backend/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package edu.uscb.csci470sp26.clinivo_backend.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,13 @@ import edu.uscb.csci470sp26.clinivo_backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
-
+	
+	List<User> findByRole(User.Role role);
+	
+	public default List<User> findPatients() {
+		return findByRole(User.Role.PATIENT);
+	}
 }
+
+
+
