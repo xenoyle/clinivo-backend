ca2e481b05ae576373b794e37491691d222c7c5c
Connor Floyd <cwfloyd@email.uscb.edu>
2026-05-03 23:42:31 -0400

    Added a patients API path and hashed the default doctor password

--- src/main/java/edu/uscb/csci470sp26/clinivo_backend/ClinivoBackendApplication.java
+++ src/main/java/edu/uscb/csci470sp26/clinivo_backend/ClinivoBackendApplication.java
@@ -4,28 +4,29 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.uscb.csci470sp26.clinivo_backend.model.User;
 import edu.uscb.csci470sp26.clinivo_backend.repository.UserRepository;
 
 @SpringBootApplication
 public class ClinivoBackendApplication {
+    
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinivoBackendApplication.class, args);
-		
-		
 	}
 		
-	@Bean
-    public CommandLineRunner dataChecker(UserRepository userRepository) {
+    @Bean
+    public CommandLineRunner defaultDoctorUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             if (userRepository.findByRole(User.Role.DOCTOR).isEmpty()) {
                 User doctor = new User();
                 doctor.setFirstName("John");
                 doctor.setLastName("Doe");
                 doctor.setEmail("doctor@test.com");
-                doctor.setPassword("password"); // test data, hash for real application
+                doctor.setPassword(passwordEncoder.encode("password"));
                 doctor.setPhoneNumber("1234567890");
                 doctor.setRole(User.Role.DOCTOR);
                 userRepository.save(doctor);
